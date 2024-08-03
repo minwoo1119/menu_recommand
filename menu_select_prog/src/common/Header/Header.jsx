928a8e36381f4ea1682fb8171d74e7cd1cee4841
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-08-03 12:12:59 +0900

    ✨ Add : Header 페이지 추가

---
+++ menu_select_prog/src/common/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './header.module.css';
+
+function Header() {
+	return (
+		<div className={styles.container}>
+			<h2>It is Header Area</h2>
+		</div>
+	);
+}
+
+export default Header;
