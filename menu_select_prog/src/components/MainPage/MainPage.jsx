70a3e3df41c5ed6e5a98a8b7ef5b6a9f46e6bb11
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-08-03 14:41:42 +0900

    🚚 Modeify : 파일 경로 수정

---
+++ menu_select_prog/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,30 @@
+import SideBar from '../SideBar/SideBar';
+import styles from './mainpage.module.css';
+import Header from '../Header/Header';
+import RandomMenu from '../RandomMenu/RandomMenu';
+
+function MainPage() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.sideBar}>
+				<SideBar />
+			</div>
+			<div className={styles.content}>
+				<Header />
+				<div className={styles.randomRecommand}>
+					<RandomMenu />
+					<RandomMenu />
+					<RandomMenu />
+				</div>
+				<div className={styles.goRecommand}>
+					<div className={styles.contentTxt}>
+						마음에 드는 메뉴가 없으신가요?
+					</div>
+					<button className={styles.goRecommandBtn}>맞춤 메뉴 추천받기</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default MainPage;
