21f9063cd379077580bf043d3484a041613d6996
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-08-08 22:51:10 +0900

    ✨ Add : routing 설정

--- menu_select_prog/src/App.jsx
+++ menu_select_prog/src/App.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import ErrorPage from './components/ErrorPage/ErrorPage';
+import RecommandPage from './components/RecommandPage/RecommandPage';
+import Recommand1 from './components/RecommandPage/Recommand1';
+import Recommand2 from './components/RecommandPage/Recommand2';
+import Recommand3 from './components/RecommandPage/Recommand3';
 
 function App() {
 	return (
@@ -11,6 +15,11 @@ function App() {
 				<BrowserRouter>
 					<Routes>
 						<Route path="/" element={<MainPage />}></Route>
+						<Route path="/recommand" element={<RecommandPage />}>
+							<Route path="/recommand" element={<Recommand1 />}></Route>
+							<Route path="/recommand/page2" element={<Recommand2 />}></Route>
+							<Route path="/recommand/page3" element={<Recommand3 />}></Route>
+						</Route>
 						<Route path="*" element={<ErrorPage />}></Route>
 					</Routes>
 				</BrowserRouter>
