03a417adbc31b7d65a956f6cb4bbd8e5eab2773d
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-08-11 16:59:17 +0900

    💄 Modify : css 스타일 오류 수정

--- menu_select_prog/src/components/RecommandPage/Recommand1.jsx
+++ menu_select_prog/src/components/RecommandPage/Recommand1.jsx
@@ -3,24 +3,31 @@ import styles from './recommand1.module.css';
 function Recommand1() {
 	return (
 		<div className={styles.container}>
-			<div className={styles.headTxt}>우선, 끌리는 카테고리가 있으신가요 ?</div>
-			<div>
+			<div className={styles.headcontainer}>
+				<div className={styles.headTxt}>
+					우선, 끌리는 카테고리가 있으신가요 ?
+				</div>
+			</div>
+			<div className={styles.grid}>
 				<div className={styles.koreanFood}>
 					<div className={styles.lableTxt}>한식</div>
-					<img src="#" alt="" />
+					<img className={styles.img} src="#" alt="" />
 				</div>
 				<div className={styles.chineseFood}>
-					<div className={styles.labelTxt}>중식</div>
-					<img src="#" alt="" />
+					<div className={styles.lableTxt}>중식</div>
+					<img className={styles.img} src="#" alt="" />
 				</div>
 				<div className={styles.italianFood}>
 					<div className={styles.lableTxt}>양식</div>
-					<img src="#" alt="" />
+					<img className={styles.img} src="#" alt="" />
 				</div>
 				<div className={styles.japaneseFood}>
-					<div className={styles.labelTxt}>일식</div>
-					<img src="#" alt="" />
+					<div className={styles.lableTxt}>일식</div>
+					<img className={styles.img} src="#" alt="" />
 				</div>
+			</div>
+			<div className={styles.nextPage}>
+				<button className={styles.nextBtn}>건너뛰기(다음)</button>
 			</div>
 		</div>
 	);
