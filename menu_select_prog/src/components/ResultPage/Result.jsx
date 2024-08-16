36e01f47eb58047f552895c5c402340b6bdf2737
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-08-16 15:05:25 +0900

    ✨ Add : FoodCard 컴포넌트 Result 페이지에 추가

--- menu_select_prog/src/components/ResultPage/Result.jsx
+++ menu_select_prog/src/components/ResultPage/Result.jsx
@@ -1,9 +1,16 @@
+import FoodCard from '../../common/foodCard/FoodCard';
 import styles from './result.module.css';
 
 function Result() {
 	return (
 		<div className={styles.container}>
-			<h1>HI</h1>
+			<div className={styles.heading}>결과</div>
+			<div className={styles.subText}>이런 음식들은 어떠세요 ?</div>
+			<div className={styles.recommands}>
+				<FoodCard />
+				<FoodCard />
+				<FoodCard />
+			</div>
 		</div>
 	);
 }
