1056fb730c2416459c62b55297e12fbd9bd80f6a
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-09-01 19:25:02 +0900

    ✨ Feat : Foodcard props 추가

--- menu_select_prog/src/common/foodCard/FoodCard.jsx
+++ menu_select_prog/src/common/foodCard/FoodCard.jsx
@@ -1,11 +1,11 @@
 import styles from './foodcard.module.css';
 
-function FoodCard() {
+function FoodCard(props) {
 	return (
 		<div className={styles.container}>
-			<div className={styles.foodname}>음식이름</div>
-			<div className={styles.foodimg}>음식 사진</div>
-			<div className={styles.description}>음식 설명</div>
+			<div className={styles.foodname}>{props.foodName}</div>
+			<div className={styles.foodimg}>{props.foodImg}</div>
+			<div className={styles.description}>{props.description}</div>
 		</div>
 	);
 }
