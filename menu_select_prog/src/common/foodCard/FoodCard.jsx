2054865aac430cebf9b87d69f02a29fed4160709
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-09-03 20:29:23 +0900

    ✨ Modify : 사진, 설명란 제거

--- menu_select_prog/src/common/foodCard/FoodCard.jsx
+++ menu_select_prog/src/common/foodCard/FoodCard.jsx
@@ -4,8 +4,6 @@ function FoodCard(props) {
 	return (
 		<div className={styles.container}>
 			<div className={styles.foodname}>{props.foodName}</div>
-			<div className={styles.foodimg}>{props.foodImg}</div>
-			<div className={styles.description}>{props.description}</div>
 		</div>
 	);
 }
