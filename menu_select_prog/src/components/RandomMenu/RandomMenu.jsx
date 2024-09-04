5ed85ba456434956de1bebd8635d65ea8012b216
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-09-04 14:08:34 +0900

    ✨ Modify : 음식 컴포넌트 수정

--- menu_select_prog/src/components/RandomMenu/RandomMenu.jsx
+++ menu_select_prog/src/components/RandomMenu/RandomMenu.jsx
@@ -3,19 +3,13 @@ import styles from './randommenu.module.css';
 function RandomMenu(props) {
 	return (
 		<div className={styles.container}>
-			<div className={styles.foodName}>음식 이름{props.name}</div>
+			<div className={styles.foodName}>{props.name}</div>
+			<div className={styles.emptybox}></div>
 			<div className={styles.img}>
 				<img src={props.imgUrl} alt="" />
 			</div>
-			<div className={styles.review}>
-				<div className={styles.likeBtn}>
-					<input type="radio" id="like" name="like" value="like" checked />
-					<label htmlFor="like">좋다</label>
-				</div>
-				<div className={styles.dislikeBtn}>
-					<input type="radio" id="dislike" name="dislike" value="dislike" />
-					<label htmlFor="dislike">별로다</label>
-				</div>
+			<div className={styles.description}>
+				<div>{props.description}</div>
 			</div>
 		</div>
 	);
